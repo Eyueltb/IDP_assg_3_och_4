5038822b591e4327377fc0fecc613e741903f85f
Eyuel <eyueltb@gmail.com>
2023-04-08 14:45:18 +0200

    initial

---
+++ molk/Assignment.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * 1. 10 poäng: Låt C# räkna ut hur många minuter det tar för oss att upptäcka det, om solen
+omedelbart slocknar och blir svart. Använd följande konstanter:
+ljushastigheten i km/s 299792.458
+solens medelavstånd i km 149598023
+sekunder per minut 60
+Deklarera och initiera variabler för dessa tre konstanter, och deklarera en variabel för
+slutresultatet, t.ex. tidIMinuterFörLjusetAttGåFrånSolenTillJorden (bara ett
+exempel!)! Skriv ut slutresultatet!
+(Slutresultatet ska visserligen bli cirka 8.317 minuter, men skriv korrekta och läsliga
+deklarationer)
+Static-metoder (funktioner):
+2. 10 poäng: Gör en funktion (static-metod) InvSqr som returnerar 1/(X*X)! Testa den med
+några värden, såsom
+2 0.25
+3 0.111111111111111 (cirka)
+4 0.0625
+5 0.04
+(det spelar ingen roll om du vid utskrift får decimalkomma eller decimalpunkt)
+Loopar (och kanske en if-sats?):
+3. 10 poäng: Gör ett program som frågar efter två tal och räknar ut summan av alla jämna tal
+mellan dem. Om vi exempelvis anger 4 och 10 så räknas summan av 4+6+8+10 ut, medan 5,
+7 och 9 ignoreras. Testa programmet med 2 och 10, 6 och 11, 7 och 9:
+mata in tal 1: 2
+mata in tal 2: 10
+summa = 30
+mata in tal 1: 6
+mata in tal 2: 11
+summa = 24
+mata in tal 1: 7
+mata in tal 2: 9
+summa = 8
+Arrayer (och några loopar):
+4. 10 poäng: Skapa en array som innehåller {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}! Gör en loop (t.ex. en
+for-loop), som förflyttar innehållet så att det ser ut så här efteråt: {2, 3, 4, 5, 6, 7, 8, 9, 10,
+1}, d.v.s. alla element utom det första flyttas ned ett steg i arrayen, medan det första läggs
+sist. Gör en loop som skriver ut arrayen!
+Static-metod:
+5. 10 poäng: Gör en funktion (static-metod) som tar två heltal m och n som argument och
+returnerar summan av alla inverser (inversen av m är 1/m) på alla heltal m, m+1, m+2 o.s.v. till
+och med n! Summan skall förstås vara av typen double. Testa programmet med 2 och 10, 6
+och 11, 7 och 9:
+mata in tal 1: 2
+mata in tal 2: 10
+summa = 1.92896825396825
+mata in tal 1: 6
+mata in tal 2: 11
+summa = 0.736544011544012
+mata in tal 1: 7
+mata in tal 2: 9
+summa = 0.378968253968254
+Objekt:
+6. 15 poäng: Djurarter har förutom sitt allmänna namn ett vetenskapligt binomialt namn
+(kallas ofta "latin" men är en ogrammatisk blanding av grekiska, latin och alla möjliga språk
+och namn på platser och personer). Exempelvis har häst (horse) det binomiala namnet
+Equus ferus – där det första ordet Equus är namn på släkte (genus) – släktesnamnet börjar
+alltid på stor bokstav – och det andra ordet ferus är namn på art (species) – artnamnet börjar
+alltid på liten bokstav.
+a. Gör en klass Species som innehåller attributen namn (eller om du föredrar engelska:
+name), släkte (genus) och art (species), samt en sträng utbredning (range) som beskriver
+en djurart.
+b. Skapa en Print-metod som skrivet ut instanser av klassen (alltså objekt)!
+c. Skapa objekt för följande arter (det behöver bara vara fyra attribut, du får välja mellan
+svenskt och engelskt namn):
+1. Vildhäst/Wild horse Equus ferus Mongoliet/Mongolia
+2. Bergszebra/Mountain zebra Equus zebra Namibia och Sydafrika/
+Namibia and South Africa
+3. Stäppzebra/Plains zebra Equus quagga Afrika/Africa
+4. Afrikansk vildåsna/
+African wild donkey
+Equus africanus Egypten, Sudan, Eritrea och
+Etiopien/
+Egypt, Sudan, Eritrea and
+Etiopia
+Skriv ut de fyra arterna. Det är en bonus om du gör en array av arter och skriver ut denna
+genom att loopa genom arrayen.
+ */
+
+using System.Linq;
+namespace molk
+{
+    internal class Assignment
+    {
+
+        public static void CalculateTimeForLightToReachEarth(double lightSpeed, double sunDistance)
+        {
+            const int secondsPerMinute = 60; // second per minute
+
+            double timeInSeconds = sunDistance / lightSpeed; // time in seconds for the light to reach the earth
+            double timeInMinutes = timeInSeconds / secondsPerMinute; // convert to minutes
+
+            Console.WriteLine($"It takes {timeInMinutes} minutes for the light to travel from the sun to the earth.");
+        }
+        
+        public static double InverseSqr(double x)
+        {
+            return 1 / (x * x);
+        }
+               
+        public static int SumAllEvenNumbersBetweenTwoNumbers(int num1, int num2)
+        {
+            int sum = 0;
+            for (int i = num1; i <= num2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        public static int SumAllEvenNumbersBetweenTwoNumbersUsingLinq(int num1, int num2) 
+        {
+            return Enumerable.Range(num1, num2 - num1 + 1)
+                     .Where(x => x % 2 == 0)
+                     .Sum();
+        }
+       
+
+        public static int[] ShiftArray(int [] arr) 
+        {
+            int first = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arr[i - 1] = arr[i];
+            }
+            arr[arr.Length - 1] = first;
+
+           return arr;
+        }
+    
+        public static double InverseSum(int m, int n)
+        {
+            double sum = 0;
+            for (int i = m; i <= n; i++)
+            {
+                sum += 1.0 / i;
+            }
+            return sum;
+        }
+       
+    }
+
+    class Species
+    {
+        public string? Name { get; set; }
+        public string? Genus { get; set; }
+        public string? SpeciesName { get; set; }
+        public string? Range { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Genus: " + Genus);
+            Console.WriteLine("Species: " + SpeciesName);
+            Console.WriteLine("Range: " + Range);
+        }
+    }
+
+    class Entry
+    {
+       
+    }
+}
+
+
+
+
---
+++ molk/Program.cs
@@ -0,0 +1,169 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using molk;
+using System;
+/**
+ * Skapa en klass Grundämne (Element på engelska) för länder med de publika attributen
+namn, Z (atomnummer), typ, smältpunkt, kokpunkt (bägge i Kelvin). Skapa ett publikt
+github-repo IDP_assg_3 för programmet.
+2. Instansiera tre grundämnen syre, järn och guld och deras data med:
+syre 8 ickemetall 54.36 90.188
+järn 26 metall 1811 3134
+guld 79 metall 1337.33 3243
+Stagea, committa och pusha!
+3. Skapa en (vanlig) publik metod Print i Grundämne som skriver ut grundämnet, ungefär så
+här:
+Grundämne: syre
+ typ: ickemetall
+ smältpunkt: 54,6 K
+ kokpunkt: 90,188 K
+Skriv ut syre, järn och guld! Stagea, committa och pusha!
+4. Skapa en array av 6 grundämnen, och tilldela 0 till 2 av arrayen värdena i syre, järn och
+guld! Direkt-tilldela de tre övriga indexen i arrayen värdena:
+väte 1 ickemetall 13.99 20.271
+brom 35 ickemetall 265.8 332.0
+kvicksilver 80 metall 234.3210 629.88
+Stagea, committa och pusha!
+5. Skapa en foreach-loop som går igenom alla element i arrayen och skriver ut länderna en
+efter en. Stagea, committa och pusha!
+
+ */
+
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        printTimeForLightToReachEarth();
+        printInverseSqure();
+        PrintSumAllEvenNumbersBetweenTwoNumbers();
+        PrintShiftArray();
+        PrintInverseSum();
+        PrintSpecies();
+    }
+    private static void printTimeForLightToReachEarth()
+    {
+        const double lightSpeed = 299792.458; // speed of light in km/s
+        const double sunDistance = 149598023; // distance of the sun in km
+        Assignment.CalculateTimeForLightToReachEarth(lightSpeed, sunDistance);
+    }
+
+    private static void printInverseSqure()
+    {
+        double[] values = { 2, 3, 4, 5 };
+        foreach (double value in values)
+        {
+            Console.WriteLine($"{value} {Assignment.InverseSqr(value)}");
+        }
+
+    }
+
+    private static void PrintSumAllEvenNumbersBetweenTwoNumbers()
+    {
+        Console.Write("Enter the first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int sum = Assignment.SumAllEvenNumbersBetweenTwoNumbers(num1, num2);
+        Console.WriteLine($"The sum of all even numbers between {num1} and {num2} is {sum}.");
+
+        sum = Assignment.SumAllEvenNumbersBetweenTwoNumbersUsingLinq(num1, num2);
+        Console.WriteLine($"The sum of all even numbers between {num1} and {num2} is {sum}.");
+
+    }
+
+    private static void PrintShiftArray()
+    {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        arr = Assignment.ShiftArray(arr);
+        Console.Write(" shifted array ");
+        foreach (int value in arr)
+        {
+            Console.Write($"{value} ");
+        }
+    }
+
+    public static void PrintInverseSum()
+    {
+        try
+        {
+            Console.WriteLine("Enter number 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            double result = Assignment.InverseSum(num1, num2);
+   
+            Console.WriteLine($"The Inverse sum of {num1} and {num2} is {result}");
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine("The entered number is too large or too small.");
+        }
+
+    }
+
+    public static void PrintSpecies()
+    {
+        
+        Species wildHorse = new()
+        {
+            Name = "Wild horse",
+            Genus = "Equus",
+            SpeciesName = "ferus",
+            Range = "Mongolia"
+        };
+
+        wildHorse.Print();
+
+        Species mountainZebra = new()
+        {
+            Name = "Mountain zebra",
+            Genus = "Equus",
+            SpeciesName = "zebra",
+            Range = "Namibia and South Africa"
+        };
+        mountainZebra.Print();
+
+        Species plainsZebra = new()
+        {
+            Name = "Plains zebra",
+            Genus = "Equus",
+            SpeciesName = "quagga",
+            Range = "Africa"
+        };
+        plainsZebra.Print();
+
+        Species africanWildDonkey = new()
+        {
+            Name = "African wild donkey",
+            Genus = "Equus",
+            SpeciesName = "africanus",
+            Range = "Egypt, Sudan, Eritrea and Ethiopia"
+        };
+        africanWildDonkey.Print();
+
+
+        Species[] speciesArray = new Species[]
+        {
+        wildHorse,
+        mountainZebra,
+        plainsZebra,
+        africanWildDonkey
+        };
+        foreach (Species species in speciesArray)
+        {
+            species.Print();
+        }
+    }
+    
+}
